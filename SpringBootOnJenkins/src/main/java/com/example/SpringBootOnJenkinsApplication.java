3f9fe57d9f93c26ca60a357aa94fb52e30f93d27
YYC <YYC@DESKTOP-RUT4CTP>
2021-04-20 12:03:40 -0500

    changes

--- SpringBootOnJenkins/src/main/java/com/example/SpringBootOnJenkinsApplication.java
+++ SpringBootOnJenkins/src/main/java/com/example/SpringBootOnJenkinsApplication.java
@@ -8,7 +8,7 @@ public class SpringBootOnJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOnJenkinsApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Hello 213");
 	}
 
 }
