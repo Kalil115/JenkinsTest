93a2193d139596d04706d2077795deb2d8d02817
YYC <YYC@DESKTOP-RUT4CTP>
2021-04-21 09:34:48 -0500

    added unit tesing case

--- src/test/java/com/example/SpringBootOnJenkinsApplicationTests.java
+++ src/test/java/com/example/SpringBootOnJenkinsApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringBootTest
 class SpringBootOnJenkinsApplicationTests {
+	
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	private UnitResource unitResource;
 
 	@Test
 	void contextLoads() {
+		mockMvc = MockMvcBuilders.standaloneSetup(unitResource).build();
+		System.out.println("Mock Mvc Objects = " + mockMvc);
+		try {
+			mockMvc.perform(MockMvcRequestBuilders.get("/unit"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().string("Unit Testing"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	
 }
